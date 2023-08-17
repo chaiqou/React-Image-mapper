bca7def2920eea92aa77e85cbb10c14c8cec9f25
nikoloz <lomtadzenikusha@gmail.com>
2023-08-17 16:16:35 +0400

    feat: Highligh first floor of apartment

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,36 @@
-import React, { useState } from "react";
-import ImageMapper from "react-img-mapper"; // Make sure to import the correct package
-
-const URL = "https://c1.staticflickr.com/5/4052/4503898393_303cfbc9fd_b.jpg";
+import React from "react";
+import ImageMapper from "react-img-mapper";
 
 const App = () => {
-  const map = {
+  const imageMapperProps = {
     name: "Redberry",
     areas: [
       {
-        name: "1",
+        name: "First floor",
         shape: "poly",
-        coords: [25, 33, 27, 300, 128, 240, 128, 94],
+        coords: [46, 357, 928, 402, 928, 473, 47, 409],
         preFillColor: "#5da0d02e",
         lineWidth: 5,
         lineColor: "red",
       },
-      {
-        name: "2",
-        shape: "poly",
-        coords: [219, 118, 220, 210, 283, 210, 284, 119],
-        lineWidth: 2,
-        strokeColor: "rgb(255, 99, 71)",
-        preFillColor: "#d05db74d",
-        fillColor: "yellow",
-      },
-      {
-        name: "3",
-        shape: "poly",
-        coords: [381, 241, 383, 94, 462, 53, 457, 282],
-        fillColor: "yellow",
-      },
-      {
-        name: "4",
-        shape: "poly",
-        coords: [245, 285, 290, 285, 274, 239, 249, 238],
-        preFillColor: "red",
-      },
-      { name: "5", shape: "circle", coords: [170, 100, 25] },
     ],
   };
 
-  return <ImageMapper src={URL} map={map} width={500} />;
+  const onClickListener = (object) => {
+    console.log(
+      `You clicked on ${object.shape} at coords ${JSON.stringify(
+        object.coords
+      )}`
+    );
+  };
+
+  return (
+    <ImageMapper
+      src="/src/assets/apartment.png"
+      map={imageMapperProps}
+      onClick={onClickListener}
+    />
+  );
 };
 
 export default App;
