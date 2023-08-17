0e2774a5fa69766bf9a883dc65ea53fa3af9b5eb
nikoloz <lomtadzenikusha@gmail.com>
2023-08-17 16:40:45 +0400

    chore: Add another floors

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,49 @@ const App = () => {
     areas: [
       {
         name: "First floor",
+        shape: "poly", // ფორმა (rect,poly,circ)
+        coords: [46, 357, 928, 402, 928, 473, 47, 409], // ფორმის კოორდინატები
+        fillColor: "rgba(229, 0, 0, 0.3)", // რა ფერი გახდება ჰოვერზე
+        strokeColor: "rgba(0, 0, 0, 0, 0)", // ბორდერის ფერი
+        lineWidth: 0, // ბორდერის სისქე
+        preFillColor: "#5da0d02e", // ჰოვერამდე ფერის მინიჭება
+        center: [30, 35, 35, 53],
+      },
+      {
+        name: "Second floor",
         shape: "poly",
-        coords: [46, 357, 928, 402, 928, 473, 47, 409],
+        coords: [46, 357, 46, 304, 929, 326, 928, 402],
+        fillColor: "rgba(229, 0, 0, 0.3)",
+        strokeColor: "rgba(0, 0, 0, 0, 0)",
+        lineWidth: 0,
         preFillColor: "#5da0d02e",
-        lineWidth: 5,
-        lineColor: "red",
+        center: [30, 35, 35, 53],
+      },
+      {
+        name: "Third floor",
+        shape: "poly",
+        coords: [46, 304, 44, 251, 931, 251, 929, 326],
+        fillColor: "rgba(229, 0, 0, 0.3)",
+        strokeColor: "rgba(0, 0, 0, 0, 0)",
+        lineWidth: 0,
+        preFillColor: "#5da0d02e",
+        center: [30, 35, 35, 53],
+      },
+      {
+        name: "Fourth floor",
+        shape: "poly",
+        coords: [44, 251, 44, 196, 933, 174, 931, 251],
+        fillColor: "rgba(229, 0, 0, 0.3)",
+        strokeColor: "rgba(0, 0, 0, 0, 0)",
+        lineWidth: 0,
+        preFillColor: "#5da0d02e",
+        center: [30, 35, 35, 53],
       },
     ],
   };
 
-  const onClickListener = (object) => {
+  const onClickEventHandler = (object) => {
+    console.log(object);
     console.log(
       `You clicked on ${object.shape} at coords ${JSON.stringify(
         object.coords
@@ -28,7 +61,7 @@ const App = () => {
     <ImageMapper
       src="/src/assets/apartment.png"
       map={imageMapperProps}
-      onClick={onClickListener}
+      onClick={onClickEventHandler}
     />
   );
 };
