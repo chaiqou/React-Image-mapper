7c040ed1fda6d4be720ab01ea81e2aa4e68676d8
chaiqou <lomtadzenikusha@gmail.com>
2023-08-21 22:06:50 +0400

    feat: Add lines with controller circles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ImageMapper from "react-img-mapper";
+import { fabric } from "fabric"; // Import Fabric.js
 import "./app.css";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     width: 0,
     height: 0,
   });
+  const [canvas, setCanvas] = useState(null); // Store the Fabric.js canvas
 
   const imageMapperProps = {
     name: "Redberry",
@@ -26,6 +28,68 @@ const App = () => {
 
   const handleImageLoad = (image) => {
     setImageDimensions({ width: image.width, height: image.height });
+
+    // Initialize Fabric.js canvas
+    const fabricCanvas = new fabric.Canvas("fabric-canvas", {
+      width: image.width,
+      height: image.height,
+    });
+    setCanvas(fabricCanvas);
+  };
+
+  const createLine = (coords) => {
+    return new fabric.Line(coords, {
+      fill: "red",
+      stroke: "red",
+      strokeWidth: 5,
+      selectable: true,
+      evented: false,
+    });
+  };
+
+  const createCircle = (left, top, line) => {
+    const circle = new fabric.Circle({
+      left: left,
+      top: top,
+      strokeWidth: 5,
+      radius: 12,
+      fill: "#fff",
+      stroke: "#666",
+    });
+    circle.hasControls = circle.hasBorders = false;
+    circle.line = line;
+
+    circle.on("moving", function () {
+      const line = this.line;
+      if (line) {
+        if (circle === line.circleLeft) {
+          line.set({ x1: circle.left, y1: circle.top });
+        } else if (circle === line.circleRight) {
+          line.set({ x2: circle.left, y2: circle.top });
+        }
+        canvas.renderAll();
+      }
+    });
+
+    return circle;
+  };
+
+  const handleAddShapes = () => {
+    if (!canvas) return;
+
+    const line = createLine([100, 100, 300, 100]);
+
+    canvas.add(line);
+
+    const circleLeft = createCircle(80, 90, line);
+    const circleRight = createCircle(290, 90, line);
+
+    line.circleLeft = circleLeft;
+    line.circleRight = circleRight;
+
+    canvas.add(circleLeft, circleRight);
+
+    canvas.renderAll();
   };
 
   return (
@@ -35,6 +99,8 @@ const App = () => {
         map={imageMapperProps}
         onLoad={handleImageLoad}
       />
+      <canvas id="fabric-canvas" className="fabric-canvas" />
+      <button onClick={handleAddShapes}>Add Shapes</button>
     </div>
   );
 };
