0109815ba1e2cb1588cdcd9e4a5bc17c9876e1bb
chaiqou <lomtadzenikusha@gmail.com>
2023-08-22 12:58:44 +0400

    refactor: back to image mapper library only

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 import ImageMapper from "react-img-mapper";
-import { fabric } from "fabric";
 import "./app.css";
 
 const App = () => {
-  const [imageDimensions, setImageDimensions] = useState({
-    width: 0,
-    height: 0,
-  });
-  const [canvas, setCanvas] = useState(null);
-  const [updatedAreas, setUpdatedAreas] = useState([]);
+  const [displayMessage, setDisplayMessage] = useState("");
+  const [coordinatesMessage, setCoordinatesMessage] = useState("");
 
   const imageMapperProps = {
     name: "Redberry",
@@ -24,91 +19,37 @@ const App = () => {
         preFillColor: "#5da0d02e",
         center: [30, 35, 35, 53],
       },
-      ...updatedAreas,
     ],
   };
 
-  const handleImageLoad = (image) => {
-    setImageDimensions({ width: image.width, height: image.height });
-
-    const fabricCanvas = new fabric.Canvas("fabric-canvas", {
-      width: image.width,
-      height: image.height,
-    });
-    setCanvas(fabricCanvas);
+  // 	Click on a zone in image
+  const onClickEventHandler = (area) => {
+    setDisplayMessage(
+      `You clicked on ${area.name} at coordinant ${JSON.stringify(area.coords)}`
+    );
   };
 
-  const createLine = (coords) => {
-    return new fabric.Line(coords, {
-      stroke: "red",
-      strokeWidth: 6,
-      selectable: true,
-      evented: true,
-    });
-  };
-
-  const handleAddShapes = () => {
-    if (!canvas) return;
-
-    const line = createLine([100, 100, 300, 100]);
-
-    canvas.add(line);
-    canvas.setActiveObject(line); // Set the line as the active object
-
-    canvas.renderAll();
-  };
-
-  const handleCalculateCoordinates = () => {
-    if (!canvas) return;
-
-    const lines = canvas
-      .getObjects()
-      .filter((obj) => obj instanceof fabric.Line);
-
-    const lineCoordinates = lines.map((line) => {
-      const { bl, br, tl, tr } = line.lineCoords;
-      return [bl.x, bl.y, br.x, br.y, tr.x, tr.y, tl.x, tl.y];
-    });
-
-    const updatedArea = {
-      name: "New Area",
-      shape: "poly",
-      coords: lineCoordinates.flat(),
-      fillColor: "rgba(229, 0, 0, 0.3)",
-      strokeColor: "rgba(0, 0, 0, 0, 0)",
-      lineWidth: 0,
-      preFillColor: "#5da0d02e",
-    };
-
-    imageMapperProps.areas.push(updatedAreas);
-    setUpdatedAreas([...updatedAreas, updatedArea]);
-    console.log(updatedArea);
-
-    console.log("Line Coordinates:", lineCoordinates);
-  };
-
-  const handleStopDrawing = () => {
-    let element = document.querySelector(".canvas-container");
-    element.parentNode.removeChild(element);
+  // Click outside of a zone in image
+  const onClickOutsideImageZoneEventHandler = (event) => {
+    const coords = { x: event.nativeEvent.layerX, y: event.nativeEvent.layerY };
+    setDisplayMessage(
+      `You clicked on the image outside zone at coords ${JSON.stringify(
+        coords
+      )} !`
+    );
   };
 
   return (
-    <div className="app-container">
-      <div className="image-mapper-container">
-        <ImageMapper
-          src="/src/assets/apartment.png"
-          map={imageMapperProps}
-          onLoad={handleImageLoad}
-        />
-      </div>
-      <button onClick={handleAddShapes}>Add Shapes</button>
-      <button onClick={handleCalculateCoordinates}>
-        Calculate Coordinates
-      </button>
-      <button onClick={handleStopDrawing}>Stop Drawing</button>
-      <div className="canvas-container">
-        <canvas id="fabric-canvas" />
-      </div>
+    <div className="image-mapper-container">
+      <ImageMapper
+        src="/src/assets/apartment.png"
+        map={imageMapperProps}
+        onClick={onClickEventHandler}
+        onImageClick={onClickOutsideImageZoneEventHandler}
+      />
+
+      <h1>{displayMessage ? displayMessage : null}</h1>
+      <h2>{coordinatesMessage ? coordinatesMessage : null}</h2>
     </div>
   );
 };
