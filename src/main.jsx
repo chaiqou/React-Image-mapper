91d473ce367d0c8850e07113812e0725a83fa32e
chaiqou <lomtadzenikusha@gmail.com>
2023-08-30 12:49:23 +0400

    chore: Add real blox render

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Vanilla from "./vanilla.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Vanilla />
   </React.StrictMode>
 );
