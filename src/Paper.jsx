17d67c2d5e4cb0f0de66551e14e4acee128b4c40
chaiqou <lomtadzenikusha@gmail.com>
2023-09-01 01:19:44 +0400

    paper: refactore code and add start and stop drawing functionality instead do everything inside first render

--- src/Paper.jsx
+++ src/Paper.jsx
@@ -1,32 +1,36 @@
-import React, { useEffect } from "react";
-import paper, { Point } from "paper";
+import React from "react";
+import paper from "paper";
 
 const Paper = () => {
-  useEffect(() => {
+  let path;
+
+  const handleStartDrawing = (event) => {
+    if (!path) {
+      path = new paper.Path({
+        strokeColor: "black",
+      });
+    }
+    path.add(event.point);
+    path.closed = true;
+    path.fullySelected = true;
+  };
+
+  const startDrawing = () => {
     paper.setup("canvas");
+    paper.view.onClick = handleStartDrawing;
+  };
 
-    let path;
+  const stopDrawing = () => {
+    paper.view.onClick = null;
+  };
 
-    const handleMouseClick = (event) => {
-      if (!path) {
-        path = new paper.Path({
-          strokeColor: "black",
-        });
-      }
-      path.add(event.point);
-      path.closed = true;
-      path.fullySelected = true;
-    };
-
-    // Attach event listener to the Paper.js canvas
-    paper.view.onClick = handleMouseClick;
-
-    return () => {
-      paper.view.onClick = null;
-    };
-  }, []);
-
-  return <canvas style={{ width: "100%", height: "100%" }} id="canvas" />;
+  return (
+    <>
+      <canvas style={{ width: "100%", height: "100%" }} id="canvas" />
+      <button onClick={startDrawing}>Start Drawing</button>
+      <button onClick={stopDrawing}>Stop Drawing</button>
+    </>
+  );
 };
 
 export default Paper;
