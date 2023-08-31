cb20e4e51991228efe9e402107d3fa2ba63f8e43
chaiqou <lomtadzenikusha@gmail.com>
2023-09-01 01:54:35 +0400

    chore: Rename files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Vanilla from "./vanilla.jsx";
+import Vanilla from "./Vanilla.jsx";
 import Paper from "./Paper.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
