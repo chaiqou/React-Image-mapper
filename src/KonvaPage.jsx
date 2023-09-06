5d3cc4ab43b37a47b7b7b5f0bf736ece68185772
chaiqou <lomtadzenikusha@gmail.com>
2023-09-06 18:10:00 +0400

    konva: install package

---
+++ src/KonvaPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const KonvaPage = () => {
+  return <div>KonvaPage</div>;
+};
+
+export default KonvaPage;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Vanilla from "./Vanilla.jsx";
 import Paper from "./Paper.jsx";
+import KonvaPage from "./KonvaPage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Paper />
+    <KonvaPage />
   </React.StrictMode>
 );
